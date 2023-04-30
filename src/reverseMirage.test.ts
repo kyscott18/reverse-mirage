4a2632a70a3020004c07984b92cd297fff92d7dc
Kyle Scott <kyscott@umich.edu>
2023-04-29 21:29:28 -0400

    internalize multicall parameters and results

--- src/reverseMirage.test.ts
+++ src/reverseMirage.test.ts
@@ -2,7 +2,6 @@ import { Token } from '@uniswap/sdk-core'
 import { createPublicClient, http } from 'viem'
 import { mainnet } from 'viem/chains'
 import { multicall } from 'viem/contract'
-import { erc20ABI } from './abi/erc20'
 import { readReverseMirages } from './index'
 import { balanceOfMirage } from './token'
 
