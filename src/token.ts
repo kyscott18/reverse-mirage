1246a94082f6caa9151f9fc25a375b4326b8c473
Kyle Scott <kyscott@umich.edu>
2023-05-16 10:27:03 -0400

    lfg

--- src/token.ts
+++ src/token.ts
@@ -1,7 +1,8 @@
-import { Address, getAddress } from 'viem'
+import { Address, Chain, PublicClient, Transport, getAddress } from 'viem'
 import { erc20ABI } from './abi/erc20'
 import { CurrencyAmount, Token } from '@uniswap/sdk-core'
 import { ReverseMirage } from './types'
+import { readReverseMirages } from '.'
 
 export const balanceOfMirage = <TToken extends Token>(
   token: TToken,
@@ -16,3 +17,75 @@ export const balanceOfMirage = <TToken extends Token>(
     },
     parse: (val) => CurrencyAmount.fromRawAmount(token, val.toString()),
   }) as const satisfies ReverseMirage<typeof erc20ABI, 'balanceOf'>
+
+export const totalSupplyMirage = <TToken extends Token>(token: TToken) =>
+  ({
+    contractConfig: {
+      functionName: 'totalSupply',
+      abi: erc20ABI,
+      address: getAddress(token.address),
+    },
+    parse: (val) => CurrencyAmount.fromRawAmount(token, val.toString()),
+  }) as const satisfies ReverseMirage<typeof erc20ABI, 'totalSupply'>
+
+export const nameMirage = <TToken extends Token>(
+  token: Pick<TToken, 'address'>,
+) =>
+  ({
+    contractConfig: {
+      functionName: 'name',
+      abi: erc20ABI,
+      address: getAddress(token.address),
+    },
+    parse: (val) => val,
+  }) as const satisfies ReverseMirage<typeof erc20ABI, 'name'>
+
+export const decimalsMirage = <TToken extends Token>(
+  token: Pick<TToken, 'address'>,
+) =>
+  ({
+    contractConfig: {
+      functionName: 'decimals',
+      abi: erc20ABI,
+      address: getAddress(token.address),
+    },
+    parse: (val) => val,
+  }) as const satisfies ReverseMirage<typeof erc20ABI, 'decimals'>
+
+export const symbolMirage = <TToken extends Token>(
+  token: Pick<TToken, 'address'>,
+) =>
+  ({
+    contractConfig: {
+      functionName: 'symbol',
+      abi: erc20ABI,
+      address: getAddress(token.address),
+    },
+    parse: (val) => val,
+  }) as const satisfies ReverseMirage<typeof erc20ABI, 'symbol'>
+
+export const getToken = async <
+  TChain extends Chain | undefined,
+  TToken extends Token,
+>(
+  client: PublicClient<Transport, TChain>,
+  token: Pick<TToken, 'address' | 'chainId'>,
+): Promise<Token> => {
+  const data = await readReverseMirages(client, {
+    contractConfig: {
+      allowFailure: false,
+      contracts: [
+        nameMirage(token).contractConfig,
+        symbolMirage(token).contractConfig,
+        decimalsMirage(token).contractConfig,
+      ],
+    },
+    parse: [
+      nameMirage(token).parse,
+      symbolMirage(token).parse,
+      decimalsMirage(token).parse,
+    ],
+  })
+
+  return new Token(token.chainId, token.address, data[2], data[1], data[0])
+}
