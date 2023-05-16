de6a4d05e9523e62ebdd29fc11e8b5ea15e6a793
Kyle Scott <kyscott@umich.edu>
2023-05-16 00:07:51 -0400

    tests

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,36 @@
-import { ContractFunctionConfig, ContractFunctionResult } from 'viem'
 import { Abi } from 'abitype'
+import { ContractFunctionConfig } from 'viem'
 
-export type ReverseMirage<
-  TAbi extends Abi | readonly unknown[] = Abi,
+type MAXIMUM_DEPTH = 20
+
+export type Contract<
+  TAbi extends Abi | readonly unknown[] = Abi | readonly unknown[],
   TFunctionName extends string = string,
-  TParse extends unknown = unknown,
-> = ContractFunctionConfig<TAbi, TFunctionName, 'view' | 'pure'> & {
-  parse: (ret: ContractFunctionResult<TAbi, TFunctionName>) => TParse
-}
+> = { abi: TAbi; functionName: TFunctionName }
+
+export type MulticallContracts<
+  TContracts extends Contract[],
+  Result extends any[] = [],
+  Depth extends readonly number[] = [],
+> = Depth['length'] extends MAXIMUM_DEPTH
+  ? ContractFunctionConfig[]
+  : TContracts extends []
+  ? []
+  : TContracts extends [infer Head extends Contract]
+  ? [...Result, ContractFunctionConfig<Head['abi'], Head['functionName']>]
+  : TContracts extends [
+      infer Head extends Contract,
+      ...infer Tail extends Contract[],
+    ]
+  ? MulticallContracts<
+      [...Tail],
+      [...Result, ContractFunctionConfig<Head['abi'], Head['functionName']>],
+      [...Depth, 1]
+    >
+  : unknown[] extends TContracts
+  ? TContracts
+  : // If `TContracts` is *some* array but we couldn't assign `unknown[]` to it, then it must hold some known/homogenous type!
+  // use this to infer the param types in the case of Array.map() argument
+  TContracts extends ContractFunctionConfig<infer TAbi, infer TFunctionName>[]
+  ? ContractFunctionConfig<TAbi, TFunctionName>[]
+  : ContractFunctionConfig[]
