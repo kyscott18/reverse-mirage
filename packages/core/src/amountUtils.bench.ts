9a7c325a4723a9774428901fc378be24cd9f619e
Kyle Scott <kyscott@umich.edu>
2023-08-19 18:05:17 -0400

    cleanup

--- packages/core/src/amountUtils.bench.ts
+++ packages/core/src/amountUtils.bench.ts
@@ -144,11 +144,4 @@ group("amount to significant", () => {
   });
 });
 
-await run({
-  avg: true, // enable/disable avg column (default: true)
-  json: false, // enable/disable json output (default: false)
-  colors: true, // enable/disable colors (default: true)
-  min_max: true, // enable/disable min/max column (default: true)
-  collect: false, // enable/disable collecting returned values into an array during the benchmark (default: false)
-  percentiles: false, // enable/disable percentiles column (default: true)
-});
+await run();
